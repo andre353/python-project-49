e93f2e6186408f43cb5ce43c88b6a96c70903760
andre353 <eva595eva@gmail.com>
2022-11-13 20:01:04 +0300

    Tweak check_user and welcome_user

--- brain_games/games/check_user.py
+++ brain_games/games/check_user.py
@@ -6,6 +6,6 @@ from brain_games.games.state_question_answer import state_question_answer
 def check_user(game):
     num_expression, right_answer = state_question_answer(game)
     print(f'Question: {num_expression}')
-    user_answer = prompt.string('Your answer? ').lower()
+    user_answer = prompt.string('Your answer? ')
     answer = check_answer(right_answer, user_answer)
     return answer
