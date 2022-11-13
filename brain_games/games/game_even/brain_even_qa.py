915b5bb419521a13a2dd875c3e55ab34228e9195
andre353 <eva595eva@gmail.com>
2022-11-13 19:35:39 +0300

    Tweak brain_even file

--- brain_games/games/game_even/brain_even_qa.py
+++ brain_games/games/game_even/brain_even_qa.py
@@ -1,10 +1,10 @@
-from brain_games.games.generate_rand_num import generate_rand_num
+import random
 
 
 def brain_even_qa():
     right_answer = ''
     num_expression = ''
-    num_expression = str(generate_rand_num())
+    num_expression = str(random.randint(1, 100))
     if (int(num_expression) % 2 == 0):
         right_answer = 'yes'
     else:
