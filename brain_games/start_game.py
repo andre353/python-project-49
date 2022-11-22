cb825621ecf15e96c66bf10e6cafcc243b5ec2a1
andre353 <eva595eva@gmail.com>
2022-11-22 16:49:40 +0300

    Update engine file

--- brain_games/start_game.py
+++ brain_games/start_game.py
@@ -12,28 +12,23 @@ def generate_rand_num(a=0, b=100):
     return random.randint(a, b)
 
 
-def welcome_user():
+def start_game(game):
     print(GREETING_PHRASE)
     name = prompt.string(GREETING_QUESTION)
     print(f"{GREETING}, {name}!")
-    return name
-
-
-def start_game(game):
-    name = welcome_user()
     print(game.TASK)
-    win_count = 0
-    while win_count < ROUNDS_TOTAL:
+    count = 0
+    while count < ROUNDS_TOTAL:
         num_expression, solution = game.module_func()
         print(f'Question: {num_expression}')
         u_anw = prompt.string('Your answer? ')
-        if (u_anw == solution):
+        if u_anw == solution:
             print("Correct!")
-            win_count += 1
+            count += 1
         else:
             print(f"{u_anw} is wrong answer ;(. Correct answer was {solution}.")
             break
-    if win_count == ROUNDS_TOTAL:
+    if count == ROUNDS_TOTAL:
         print(f'Congratulations, {name}!')
     else:
         print(f"Let\'s try again, {name}!")
